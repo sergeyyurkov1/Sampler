1f7d51ad6baa51495aa796b7c664db6a82632258
Sergey Yurkov <38420364+sergeyyurkov1@users.noreply.github.com>
2021-12-05 05:25:26 +0500

    Update sampler.py

--- sampler.py
+++ sampler.py
@@ -9,38 +9,29 @@ import fitz
 CUR_DIR = os.path.dirname(__file__)
 OUT_DIR = "SAMPLE"
 
-week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-exts = ["rtf", "pdf"]
+EXTS = ["rtf", "pdf"]
 
-num_constructed_weeks = 2
+num_constructed_weeks = 1
 
-def main():
-    dirs = week + [OUT_DIR] # dirs to create
-    for dir_ in dirs:
-        if not os.path.isdir(dir_):
-            os.mkdir(dir_)
-        else:
-            print("Sampling had already been done!")
-            exit()
-    
+def main():    
     sort()
     sample()
 
 def sort():
     """Finds and sorts all rtf or pdf files in the working directory by weekday"""
-    
     files = []
 
     dir_listing = os.scandir()
     for i in dir_listing:
-        if i.is_file() and i.name.split(".")[-1].lower() in exts:
+        if i.is_file() and i.name.split(".")[-1].lower() in EXTS:
             files.append(i.name)
     dir_listing.close()
 
     if len(files) > 0:
         for e, i in enumerate(files):
-            ext = i.name.split(".")[-1].lower()
+            ext = i.split(".")[-1].lower()
             if ext == "rtf":
                 with open(i, "r") as f:
                     text = f.read()
@@ -52,40 +43,61 @@ def sort():
                 f.close()
 
             # Extracts weekday from the following string format: "February 4, 2021 Thursday 7:45 AM GMT" (as found in Lexis docs)
-            weekday = re.search(r"[a-zA-Z]+\s\d{1,2},\s\d{4}\s([a-zA-Z]+)\s\d{1,2}:\d{2}\s[A-Z]{2}\s[A-Z]{3}", text).group(1)
+            date, weekday = re.search(r"([a-zA-Z]+\s\d{1,2}),\s\d{4}\s([a-zA-Z]+)\s\d{1,2}:\d{2}\s[A-Z]{2}\s[A-Z]{3}", text).groups()
             
             # Sorting docs by weekday
             print(f"Sorting {e+1}/{len(files)}") # simple progress bar
-            shutil.move(os.path.join(CUR_DIR, i), os.path.join(CUR_DIR, f"{weekday}/{i}")) # moves the doc to its respective weekday folder
+            from_ = os.path.join(CUR_DIR, i)
+            to = os.path.join(CUR_DIR, f"{weekday}/{date}/{i}")
+            os.makedirs(os.path.dirname(to), exist_ok=True)
+            shutil.move(from_, to) # moves the doc to its respective weekday/date folder
     else:
         print("No files to sort!")
         exit()
 
 def sample():
-    """Performs constructed week sampling from sorted documents with n-weeks"""
-
+    """Performs constructed WEEK sampling from sorted documents with n-weeks"""
     print("Sampling...")
 
-    for day in week:
+    for day in WEEK:
         hat = []
 
-        dir_listing = os.scandir(os.path.join(CUR_DIR, day))
-        for i in dir_listing:
-            if i.is_file():
-                hat.append(i.name)
-        dir_listing.close()
+        try:
+            dir_listing = os.scandir(os.path.join(CUR_DIR, day))
+            for i in dir_listing:
+                if i.is_dir():
+                    hat.append(i.name)
+            dir_listing.close()
+        except FileNotFoundError:
+            print(f"Warning: no documents found for {day}")
+            break
 
-        # Checks whether the number of docs on a given day is enough for sampling, otherwise uses the whole day's population or skips if no docs are found (len(hat) == 0)
+        # Checks whether the number of docs is enough for sampling n-weeks, otherwise uses the whole population, or errors out if no docs are found
         if len(hat) >= num_constructed_weeks:
             sample = random.sample(hat, num_constructed_weeks)
             for n in sample:
-                shutil.move(os.path.join(CUR_DIR, f"{day}/{n}"), os.path.join(CUR_DIR, f"{OUT_DIR}/{n}")) # Moves sampled docs to the output directory
+                dir_listing = os.scandir(os.path.join(CUR_DIR, day, n))
+                for i in dir_listing:
+                    if i.is_file():
+                        from_ = os.path.join(CUR_DIR, day, n, i.name)
+                        to = os.path.join(CUR_DIR, OUT_DIR, i.name)
+                        os.makedirs(os.path.dirname(to), exist_ok=True)
+                        shutil.move(from_, to) # Moves sampled docs to the output directory
+                dir_listing.close()
         elif 0 < len(hat) < num_constructed_weeks:
             print(f"Warning: using whole day's population for {day}")
             for N in hat:
-                shutil.move(os.path.join(CUR_DIR, f"{day}/{N}"), os.path.join(CUR_DIR, f"{OUT_DIR}/{N}")) # Moves docs to the output directory
-        else:
-            print(f"Warning: no documents found for {day}")
+                dir_listing = os.scandir(os.path.join(CUR_DIR, day, N))
+                for i in dir_listing:
+                    if i.is_file():
+                        from_ = os.path.join(CUR_DIR, day, N, i.name)
+                        to = os.path.join(CUR_DIR, OUT_DIR, i.name)
+                        os.makedirs(os.path.dirname(to), exist_ok=True)
+                        shutil.move(from_, to) # Moves sampled docs to the output directory
+                dir_listing.close()
+        else: # if 0 documents
+            print(f"Error: nothing to sample!")
+            exit()
 
     print("Done!")
 
